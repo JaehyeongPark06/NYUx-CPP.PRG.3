20c86925c3e885129ff91f796ac8cac5d242f5dc
Jaden (Jaehyeong) Park <78674944+JaehyeongPark06@users.noreply.github.com>
2022-11-06 19:51:34 -0500

    Create addone.cpp

---
+++ addone.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void addOne(int *ptrNum){
+    ++*ptrNum;
+};
+
+int main(){
+    int num = 0;
+    int* numPtr = #
+    addOne(numPtr);
+    cout << num << endl;
+
+    return 0;
+};
